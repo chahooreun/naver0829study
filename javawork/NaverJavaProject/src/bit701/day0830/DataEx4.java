8d31a575bf4d516c06fb61da117e1bb7dccf16a8
chahooreun <ckdlgnfus@naver.com>
2023-08-30 16:18:06 +0900

    자바연습

---
+++ javawork/NaverJavaProject/src/bit701/day0830/DataEx4.java
@@ -0,0 +1,18 @@
+package bit701.day0830;
+
+public class DataEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a1=0xa3;//10진수로 얼마???? -
+		System.out.println(a1);
+		
+		int a2=0xf4;
+		System.out.println(a2);
+		
+		int a3=0xa56;
+		System.out.println(a3);
+	}
+
+}
